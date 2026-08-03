e6da14d746e0e9676186ec81c176d6e73d345e1a
juancruzbucchioni-cyber <juancruzbucchioni-cyber@users.noreply.github.com>
2026-08-02 23:16:49 -0300

    Update digital ecommerce price

--- app/page.tsx
+++ app/page.tsx
@@ -147,8 +147,8 @@ const plans = [
     category: "ventas-digitales",
     type: "Web E-commerce Digital",
     tag: "VENTAS DIGITALES",
-    price: "230.000",
-    amount: 230000,
+    price: "250.000",
+    amount: 250000,
     color: "blue",
     previews: [
       "/digital-gallery-01.png",
